719743de6409334a621b5bed7c39e24d4a4d8b67
Ilya Ivanov <ilyaivanov101@yandex.ru>
2023-04-27 15:09:34 +0300

    JUnit 4

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testAmountBefore1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+        int actual = service.remain(amount);
+        int expected = 200;
+
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void testAmount1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void testAmountMore1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1500;
+        int actual = service.remain(amount);
+        int expected = 500;
+
+        assertEquals(actual,expected);
+    }
+}
