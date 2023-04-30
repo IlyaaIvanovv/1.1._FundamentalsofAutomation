58679bb0ca55e77336c6a96b4cb36e3333b1ce05
Ilya Ivanov <ilyaivanov101@yandex.ru>
2023-04-30 11:24:04 +0300

    Fix Assert

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -7,29 +7,29 @@ public class CashbackHackServiceTest {
     public void testAmountBefore1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 800;
-        int actual = service.remain(amount);
         int expected = 200;
+        int actual = service.remain(amount);
 
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
 
     @Test
     public void testAmount1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
-        int actual = service.remain(amount);
         int expected = 0;
+        int actual = service.remain(amount);
 
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
 
     @Test
     public void testAmountMore1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1500;
-        int actual = service.remain(amount);
         int expected = 500;
+        int actual = service.remain(amount);
 
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
 }
